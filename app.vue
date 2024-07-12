c76304d6f4b153007cb2bb2ed7673869fc040487
lxqddd <xiangqian_liu@163.com>
2024-07-12 23:33:27 +0800

    style: add some base style

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage :transition="{
+        name: 'bounce',
+        mode: 'out-in'
+      }" />
+    </NuxtLayout>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="h-100vh bg-[var(--ui-background)] text-[var(--ui-foreground)]">
     <slot />
   </div>
 </template>
