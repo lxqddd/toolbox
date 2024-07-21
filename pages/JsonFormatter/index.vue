31cf3a74fb75f12647aec12ff66d8e6088ee5cdf
xy <xiangqian_liu@163.com>
2024-07-21 22:51:56 +0800

    feat: json beauty

--- pages/JsonFormatter/index.vue
+++ pages/JsonFormatter/index.vue
@@ -4,12 +4,12 @@
        <MonacoEditor class="h-100%" v-model="prevPrettierJson" lang="json" :options="MonacoEditOptions"/>
     </div>
     <div ref="prettiedEditRef" class="flex-1">
-      <MonacoEditor class="h-100%" v-model="prevPrettierJson" lang="json" :options="MonacoEditOptions"/>
+      <MonacoEditor class="h-100%" v-model="prettiedJson" lang="json" :options="MonacoEditOptions"/>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 import type * as monaco from 'monaco-editor'
 
 const MonacoEditOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
@@ -30,9 +30,20 @@ const MonacoEditOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
   snippetSuggestions: 'inline'
 }
 const prevPrettierJson = ref('')
+const prettiedJson = ref('')
 
 
-watch(prevPrettierJson, (newVal, oldVal) => {
-  console.log('newVal', newVal)
+watch(prevPrettierJson, async (newVal, oldVal) => {
+  prettiedJson.value = await formatJson(newVal)
 })
+
+
+const formatJson = async (jsonStr: string) => {
+  try {
+    return JSON.stringify(JSON.parse(jsonStr), null, 2)
+  } catch (error) {
+    return (error as Error).message
+  }
+}
+
 </script>
