2f2611e216129d94d06db95000d1595e8f36887a
xy <xiangqian_liu@163.com>
2024-08-31 17:24:13 +0800

    feat: deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@unocss/nuxt', "nuxtjs-naive-ui", 'nuxt-monaco-editor'],
@@ -17,6 +18,13 @@ export default defineNuxtConfig({
     baseURL: process.env.NODE_ENV === 'production' ? 'toolbox' : '/',
     layoutTransition: { name: 'layout', mode: 'out-in' },
     buildAssetsDir: 'nuxt_assets'
+  },
+  nitro: {
+    prerender: {
+      // routes: ['JsonFormatter'],
+      ignore: ['/404.html', '/200.html', '/EnhanceImage'],
+      crawlLinks: true
+    }
   },
   experimental: {
     payloadExtraction: false,
